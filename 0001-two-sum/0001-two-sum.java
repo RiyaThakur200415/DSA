d89c7812104af68d2562fb949bb7df891b3d146c
Riya Kumari <riya.kri.thakur2004@gmail.com>
2026-05-30 18:32:39 +0530

    Time: 2 ms (99.39%), Space: 47.1 MB (39.65%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,38 +1,19 @@
-import java.util.*;
 class Solution 
 {
-    public static int[] twoSum(int[] nums, int target) 
+    public int[] twoSum(int[] nums, int target) 
     {
-        int n=nums.length;
-        int result[]=new int[2];
-        for(int i=0;i<n;i++)
+        HashMap<Integer , Integer> hm = new HashMap<>();
+
+        for(int i = 0 ; i < nums.length ; i++)
         {
-            for(int j=1+i;j<n;j++)
+            int complement = target - nums[i];
+
+            if(hm.containsKey(complement))
             {
-                int sum=nums[i]+nums[j];
-                if(sum==target)
-                {
-                    int fidx=i;
-                    int sidx=j;
-                    result[0]=fidx;
-                    result[1]=sidx;
-                }
+                return new int[]{hm.get(complement) , i};
             }
+            hm.put(nums[i] , i);
         }
-        
-        return result;
-    }
-    public static void main(String args[])
-    {
-        Scanner sc=new Scanner(System.in);
-        int n;
-        n=sc.nextInt();
-        int nums[]=new int[n];
-        for(int i=0;i<n;i++)
-        {
-            nums[i]=sc.nextInt();
-        }
-        int target=sc.nextInt();
-        System.out.println(twoSum(nums,target));
+        return new int[]{};
     }
 }
